505a3a11351646a073fb009173820c5de73a0337
thaianh26922 <anhpt@solashi.com>
2025-06-02 06:05:42 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    outDir: 'dist'
-  }
-})
+  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+});
